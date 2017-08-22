85c5be24a86bbe37777e5a29b2ee58094878f12d
Владимир Петров <vmpetrov@edu.hse.ru>
2017-08-22 21:51:59 +0500

    Запилил прогу

--- The task 4/Program.cs
+++ The task 4/Program.cs
@@ -10,6 +10,27 @@ namespace The_task_4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите погрешность: ");
+            double e;
+            bool ok;
+
+            do { ok = double.TryParse(Console.ReadLine(), out e); if (!ok || e < 0) { Console.WriteLine("Ошибка ввода"); } } while (!ok);
+
+            double a = -1, b = 0;
+            while (Math.Abs(b - a) > e)
+            {
+                a = b - (b - a) * f(b) / (f(b) - f(a));
+                b = a + (a - b) * f(a) / (f(a) - f(b));
+            }
+
+            Console.WriteLine("Ответ: " + b);
+            Console.ReadKey();
+        }
+
+        public static double f(double x_)
+        {
+            double x = x_;
+            return x * x * x * x + 0.5 * x * x * x - 4 * x * x - 3 * x - 0.5;
         }
     }
 }
